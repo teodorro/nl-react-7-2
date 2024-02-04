76579d4cc4c19431db8708d10adb117ba6a677e3
theo <edem.teo@gmail.com>
2024-02-04 17:04:57 +0300

    HOC

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,126 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import "./css/index.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [list, setList] = useState([
+    {
+      type: "video",
+      url: "https://www.youtube.com/embed/rN6nlNC9WQA?rel=0&amp;controls=0&amp;showinfo=0",
+      views: 50,
+    },
+    {
+      type: "video",
+      url: "https://www.youtube.com/embed/dVkK36KOcqs?rel=0&amp;controls=0&amp;showinfo=0",
+      views: 12,
+    },
+    {
+      type: "article",
+      title: "Невероятные события в неизвестном поселке...",
+      views: 175,
+    },
+    {
+      type: "article",
+      title: "Секретные данные были раскрыты!",
+      views: 1532,
+    },
+    {
+      type: "video",
+      url: "https://www.youtube.com/embed/TKmGU77INaM?rel=0&amp;controls=0&amp;showinfo=0",
+      views: 4253,
+    },
+    {
+      type: "article",
+      title: "Кот Бегемот обладает невероятной...",
+      views: 12,
+    },
+  ]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return <List list={list} />;
 }
 
-export default App
+function New(props) {
+  return (
+    <div className="wrap-item wrap-item-new">
+      <span className="label">New!</span>
+      {props.children}
+    </div>
+  );
+}
+
+function Popular(props) {
+  return (
+    <div className="wrap-item wrap-item-popular">
+      <span className="label">Popular!</span>
+      {props.children}
+    </div>
+  );
+}
+
+function Article(props) {
+  return (
+    <div className="item item-article">
+      <h3>
+        <a href="#">{props.title}</a>
+      </h3>
+      <p className="views">Прочтений: {props.views}</p>
+    </div>
+  );
+}
+
+function Video(props) {
+  return (
+    <div className="item item-video">
+      <iframe
+        src={props.url}
+        frameBorder="0"
+        allow="autoplay; encrypted-media"
+        allowfullscreen
+      ></iframe>
+      <p className="views">Просмотров: {props.views}</p>
+    </div>
+  );
+}
+
+function List(props) {
+  return props.list.map((item) => {
+    if (item.views > 1000) {
+      if (item.type === "video") {
+        const prop = {
+          children: (<Video {...item} />)
+        }
+        return <Popular {...prop} />;
+      }
+      if (item.type === "article") {
+        const prop = {
+          children: (<Article {...item} />)
+        }
+        return <Popular {...prop} />;
+      }
+    }
+    if (item.views < 100) {
+      if (item.type === "video") {
+        const prop = {
+          children: (<Video {...item} />)
+        }
+        return <New {...prop} />;
+      }
+      if (item.type === "article") {
+        const prop = {
+          children: (<Article {...item} />)
+        }
+        return <New {...prop} />;
+      }
+    }
+    switch (item.type) {
+      case "video":
+        return <Video {...item} />;
+
+      case "article":
+        return <Article {...item} />;
+    }
+    return null;
+  });
+}
+
+export default App;
